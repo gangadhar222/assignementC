abf915fb54123f44b62e312e4b61ed5eb6cd12c6
gangadhar222 <gangadhar.eee@hotmail.com>
2020-08-28 17:12:08 +0530

    upto back-ground part

---
+++ loginpage/src/Components/LoginPage.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './LoginPage.css';
+
+
+export class LoginPage extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+                <div className="image">
+                    <div className="form">
+                        
+                    </div>
+                </div>
+        )
+    }
+}
+
+export default LoginPage
